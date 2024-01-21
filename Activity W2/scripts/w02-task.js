d2c002a6c3e4a8060f246776f153b0deaf3a975b
Nam3Mc <mor23083@byui.edu>
2024-01-21 18:15:04 -0500

    done

--- Activity W2/scripts/w02-task.js
+++ Activity W2/scripts/w02-task.js
@@ -19,16 +19,9 @@ imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
-const favoriteFoods = ['Pizza', 'Ice Cream', 'Sushi']; // Add your favorite foods
-const newFavoriteFood = 'Burger'; // Add another favorite food
+const favoriteFoods = ['Pizza', 'Ice Cream', 'Sushi']; 
+const newFavoriteFood = 'Burger'; 
 
-favoriteFoods.push(newFavoriteFood); // Add the new favorite food to the array
+favoriteFoods.push(newFavoriteFood); 
 
-// Display the array content in the HTML element with the id of 'food'
-foodElement.innerHTML += favoriteFoods.join('<br>');
-
-// Remove the first element in the favorite food array
-favoriteFoods.shift();
-
-// Display the modified array content in the HTML element with the id of 'food'
-foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
+foodElement.innerHTML += favoriteFoods.join('<br>')
